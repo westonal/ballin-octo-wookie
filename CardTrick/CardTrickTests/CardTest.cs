dbae268a812b1d271588f7150e241e9efb557a71
Alan Evans <alan.evans@burrows.yr.com>
2014-03-28 14:11:45 +0000

    To and from string.

--- CardTrick/CardTrickTests/CardTest.cs
+++ CardTrick/CardTrickTests/CardTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Cards;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -26,8 +27,53 @@ namespace CardTrickTests
         }
 
         [TestMethod]
-        [Ignore]
         public void Can_create_from_string()
+        {
+            var expected = new Card(Suit.Club, CardValue.Ace);
+            Assert.AreEqual(expected, Card.FromString("AC"));
+        }
+
+        [TestMethod]
+        public void Can_create_from_string_reversed()
+        {
+            var expected = new Card(Suit.Club, CardValue.Ace);
+            Assert.AreEqual(expected, Card.FromString("CA"));
+        }
+
+        [TestMethod]
+        public void Can_create_two_from_string()
+        {
+            var expected = new Card(Suit.Club, CardValue.Two);
+            Assert.AreEqual(expected, Card.FromString("2C"));
+        }
+
+        [TestMethod]
+        public void Can_create_two_from_string_reversed()
+        {
+            var expected = new Card(Suit.Club, CardValue.Two);
+            Assert.AreEqual(expected, Card.FromString("2C"));
+        }
+
+        [TestMethod]
+        public void To_string()
+        {
+            Assert.AreEqual("2C", new Card(Suit.Club, CardValue.Two).ToString());
+        }
+
+        [TestMethod]
+        public void Can_create_all_clubs_from_string()
+        {
+            const Suit suit = Suit.Club;
+            foreach (CardValue cardValue in Enum.GetValues(typeof (CardValue)))
+            {
+                var expected = new Card(suit, cardValue);
+                var asString = expected.ToString();
+                Assert.AreEqual(expected, Card.FromString(asString));
+            }
+        }
+
+        [TestMethod]
+        public void Can_create_all_from_string()
         {
             foreach (Suit suit in Enum.GetValues(typeof (Suit)))
             {
--- CardTrick/Cards/Card.cs
+++ CardTrick/Cards/Card.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Globalization;
+
 namespace Cards
 {
     public sealed class Card
@@ -13,12 +16,87 @@ namespace Cards
 
         public override string ToString()
         {
-            return string.Format("{0} {1}", CardValue, Suit);
+            return string.Format("{0}{1}", ToString(CardValue), ToString(Suit));
+        }
+
+        private string ToString(Suit suit)
+        {
+            return suit.ToString()[0].ToString(CultureInfo.InvariantCulture);
+        }
+
+        private string ToString(CardValue cardValue)
+        {
+            switch (cardValue)
+            {
+                case CardValue.Ace:
+                    return "A";
+                case CardValue.Ten:
+                    return "T";
+                case CardValue.Jack:
+                    return "J";
+                case CardValue.King:
+                    return "K";
+                case CardValue.Queen:
+                    return "Q";
+                default:
+                    return ((int) cardValue + 1).ToString(CultureInfo.InvariantCulture);
+            }
         }
 
         public static Card FromString(string asString)
         {
-            return new Card(Suit.Club, CardValue.Ace);
+            var suit = FindSuit(asString);
+            var value = FindValue(asString);
+
+            return new Card(suit, value);
+        }
+
+        private static CardValue FindValue(string asString)
+        {
+            if (asString.Contains("2"))
+                return CardValue.Two;
+            if (asString.Contains("3"))
+                return CardValue.Three;
+            if (asString.Contains("4"))
+                return CardValue.Four;
+            if (asString.Contains("5"))
+                return CardValue.Five;
+            if (asString.Contains("6"))
+                return CardValue.Six;
+            if (asString.Contains("7"))
+                return CardValue.Seven;
+            if (asString.Contains("8"))
+                return CardValue.Eight;
+            if (asString.Contains("9"))
+                return CardValue.Nine;
+            if (asString.Contains("T"))
+                return CardValue.Ten;
+            if (asString.Contains("10"))
+                return CardValue.Ten;
+            if (asString.Contains("J"))
+                return CardValue.Jack;
+            if (asString.Contains("Q"))
+                return CardValue.Queen;
+            if (asString.Contains("K"))
+                return CardValue.King;
+            if (asString.Contains("A"))
+                return CardValue.Ace;
+            if (asString.Contains("1"))
+                return CardValue.Ace;
+            throw new Exception("Value not specified, please use A,2-9,T,J,K,Q");
+        }
+
+        private static Suit FindSuit(string asString)
+        {
+            if (asString.Contains("C"))
+                return Suit.Club;
+            if (asString.Contains("D"))
+                return Suit.Diamond;
+            if (asString.Contains("H"))
+                return Suit.Heart;
+            if (asString.Contains("S"))
+                return Suit.Spade;
+            throw new Exception("Suit not specified, please use C,H,D or S");
         }
 
         public override bool Equals(object obj)
