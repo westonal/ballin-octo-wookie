7358573c2f7a6b5865e26e0a8d65660111cee24f
Alan Evans <thealanevans@googlemail.com>
2014-03-30 18:36:19 +0100

    For passing more than once, it doesn't seem to be enough to know half the deck.

--- CardTrick/CardTrickTests/CardTrickTest.cs
+++ CardTrick/CardTrickTests/CardTrickTest.cs
@@ -71,22 +71,68 @@ namespace CardTrickTests
             var deck = new Deck();
             var model = new Model { ActualDeck = deck.Serialize(), DeckKnowledge = deck.Serialize() };
             var random = new Random(512);
-            PerformTrick(model, random);
+            PerformTrick(model, random, 1);
         }
 
         [TestMethod]
         public void Can_perform_trick_twice_with_learning()
         {
+            for (int seed = 1; seed <= 40; seed++)
+            {
+                //setup
+                var deck = new Deck();
+                var model = new Model { ActualDeck = deck.Serialize(), DeckKnowledge = deck.Serialize() };
+
+                var random = new Random(seed);
+
+                for (int i = 1; i <= 2; i++)
+                {
+                    model = PerformTrick(model, random, seed * 100 + i);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Can_perform_trick_twice_without_learning()
+        {
+            for (int seed = 1; seed <= 40; seed++)
+            {
+                //setup
+                var deck = new Deck();
+                var model = new Model { ActualDeck = deck.Serialize(), DeckKnowledge = deck.Serialize() };
+
+                var random = new Random(seed);
+
+                for (int i = 1; i <= 2; i++)
+                {
+                    var newModel = PerformTrick(model, random, seed * 100 + i);
+                    Assert.AreNotEqual(newModel.ActualDeck, model.ActualDeck);
+                    model = new Model
+                    {
+                        ActualDeck = newModel.ActualDeck,
+                        DeckKnowledge = model.DeckKnowledge
+                    };
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Can_perform_trick_three_times_without_learning()
+        {
             //setup
             var deck = new Deck();
             var model = new Model { ActualDeck = deck.Serialize(), DeckKnowledge = deck.Serialize() };
-            var random = new Random(512);
+            var random = new Random(5981951);
 
-            for (int i = 0; i < 2; i++)
+            for (int i = 1; i <= 3; i++)
             {
-                model = PerformTrick(model, random);
-                //Assert.AreNotEqual(newDeck, deck);
-                //deck = newDeck;
+                var newModel = PerformTrick(model, random, i);
+                Assert.AreNotEqual(newModel.ActualDeck, model.ActualDeck);
+                model = new Model
+                {
+                    ActualDeck = newModel.ActualDeck,
+                    DeckKnowledge = model.DeckKnowledge
+                };
             }
         }
 
@@ -96,7 +142,7 @@ namespace CardTrickTests
             public string DeckKnowledge { get; set; }
         }
 
-        private Model PerformTrick(Model model, Random random)
+        private Model PerformTrick(Model model, Random random, int run)
         {
             var trick = new Trick(model.DeckKnowledge);
             var trick2 = new Trick(model.DeckKnowledge);
@@ -106,19 +152,20 @@ namespace CardTrickTests
             //trick steps
             deck.Manipulate(new Cut(20));
             deck.Manipulate(new RiffleShuffle());
+            var newDeck = deck.Serialize();
             var deck2 = Deck.NewEmptyDeck();
             deck.Manipulate(new Cut(26, deck2));
 
             Assert.AreEqual(26, deck.Count());
             Assert.AreEqual(26, deck2.Count());
 
-            var card = deck.TakeCard(random.Next(2, 23));
-            deck2.InsertCard(card, random.Next(2, 11));
+            var card = deck.TakeCard(random.Next(3, 23));
+            deck2.InsertCard(card, random.Next(3, 23));
 
-            Assert.AreEqual(card, trick.FindCard(deck));
-            Assert.AreEqual(card, trick2.FindCard(deck2));
+            Assert.AreEqual(card, trick.FindCard(deck), "Run " + run);
+            Assert.AreEqual(card, trick2.FindCard(deck2), "Run " + run);
 
-            return new Model { ActualDeck = deck.Serialize() + deck2.Serialize(), DeckKnowledge = model.DeckKnowledge };
+            return new Model { ActualDeck = newDeck, DeckKnowledge = deck.Serialize() + model.DeckKnowledge };
         }
     }
 }
